5e079dd12dd6e9550af00062bd5132474276e62a
Pooja jat <patelpoojajat@gmail.com>
2025-02-17 21:48:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("button");
+
+function fetchJoke() {
+  btn.disabled = true;
+
+  fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("joke").innerText = data.setup;
+      document.getElementsByClassName("para")[0].innerText = data.punchline;
+      btn.disabled = false;
+    })
+    .catch((error) => {
+      document.getElementById("joke").innerText = "error";
+      console.error("Error fetching joke:", error);
+      btn.disabled = false;
+    });
+}
+
+fetchJoke();
+
+btn.addEventListener("click", fetchJoke);
